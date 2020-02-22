c1608acf1679d518d4ee6d27afe94547fd1402ee
Natacha <nabarcala@gmail.com>
2020-02-21 23:35:48 -0500

    begining of login page

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 # Routes module
+from flask import render_template, flash, redirect
 from app import app
-from flask import render_template
+from app.forms import LoginForm
 
+# Index/Home 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    user={'username': 'Natacha'}
-    posts=[
+    user = {'username': 'Natacha'}
+    posts = [
         {
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
@@ -18,3 +19,14 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    # Accept and validate the data, then redirect if valid
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember me={}'.format(
+        form.username.data, form.remember_me.data))
+        return redirect('/index')
+    
+    return render_template('login.html', title='Sign In', form=form)
