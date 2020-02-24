db0fbb06f828175f367a1ada8f3ab947bbcc07c6
Natacha <nabarcala@gmail.com>
2020-02-23 20:13:25 -0500

    Update init with database imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 #!../venv/bin/python3
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Database
+db = SQLAlchemy(app)
+# Migration engine
+migrate = Migrate(app, db)
+
+from app import routes, models
