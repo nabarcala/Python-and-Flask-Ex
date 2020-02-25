13fc05c91b8b1e359a7d17df5ca9fc842b837fcb
Natacha <nabarcala@gmail.com>
2020-02-25 16:07:36 -0500

    Add login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 # Routes module
 from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 # Index/Home 
 @app.route('/')
@@ -20,13 +22,22 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
+# Login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Redirect if user is logged in already
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     # Accept and validate the data, then redirect if valid
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember me={}'.format(
-        form.username.data, form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        # Check if password is correct
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        # If passes check, log user in
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     
     return render_template('login.html', title='Sign In', form=form)
