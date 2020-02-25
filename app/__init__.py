a092451b5f25980982909ba77da0cb45dada3e42
Natacha <nabarcala@gmail.com>
2020-02-25 15:38:44 -0500

    Add password hashing

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
-#!../venv/bin/python3
+
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +12,13 @@ class User(db.Model):
     # How to print objects of this class
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    # Set a hashed password using werkzeug
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
