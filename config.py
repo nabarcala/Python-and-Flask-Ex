b4f66a2a9c0ab8fe7bf7fa9e89dc8ad3558141bf
Natacha <nabarcala@gmail.com>
2020-03-20 16:12:51 -0400

    Add pagination for explore page

--- config.py
+++ config.py
@@ -16,3 +16,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['nabarcala@gmail.com']
+    
+    POSTS_PER_PAGE = 6
