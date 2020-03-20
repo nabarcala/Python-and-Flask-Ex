2002d7c59de2ee90a2c18ab905431c38844ab191
Natacha <nabarcala@gmail.com>
2020-03-20 12:45:49 -0400

    Basics of user profile

--- app/routes.py
+++ app/routes.py
@@ -70,9 +70,16 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
-
-
-
+# User's profile
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
 
 
 
