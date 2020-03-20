7760d46372a9fd4b728f30a8011c893b083cab57
Natacha <nabarcala@gmail.com>
2020-03-20 13:33:24 -0400

    Add ability for user to edit their about me profile section

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+from app.forms import EditProfileForm
 from werkzeug.urls import url_parse
 from datetime import datetime
 
@@ -89,7 +90,21 @@ def before_request():
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
 
-
+# Ability to edit profile
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
 
 
 
