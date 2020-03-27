727ee0b4a7b33e75f0070ec2e24aea709625798d
Natacha <nabarcala@gmail.com>
2020-03-26 22:40:00 -0400

    Add is_admin flag

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ followers = db.Table('followers',
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    is_admin = db.Column(db.Boolean, default=False)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
