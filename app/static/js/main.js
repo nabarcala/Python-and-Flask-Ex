99082d71ad9116c2106caf56d630b609e80736b8
Natacha <nabarcala@gmail.com>
2020-07-07 20:15:38 -0400

    Added scroll to top button

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,18 +2,18 @@
 // Menu Show
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+    nav = document.getElementById(navId);
 
     if(toggle && nav) {
         toggle.addEventListener('click', ()=> {
             nav.classList.toggle('show')
         })
     }
-}
-showMenu('nav-toggle', 'nav-menu')
+};
+showMenu('nav-toggle', 'nav-menu');
 
 // Active and remove active menu
-const navLink = document.querySelectorAll('.nav-link')
+const navLink = document.querySelectorAll('.nav-link');
 
 function linkAction() {
     // Active link
@@ -23,6 +23,22 @@ function linkAction() {
     // remove menu mobile
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
-}
+};
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// Scroll to top button
+const btnScrollToTop = document.querySelector('#scroll-to-top');
+
+btnScrollToTop.addEventListener("click", function () {
+    // may not work in all browsers
+    // window.scrollTo( {
+    //     top: 0,
+    //     left: 0,
+    //     behavior: "smooth"
+    // });
+
+    //
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+});
