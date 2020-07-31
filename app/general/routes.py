4dc5b59019cb32507bc8243fd4435ae52c448071
Natacha <nabarcala@gmail.com>
2020-07-31 15:07:47 -0400

    Change portfolio layout.

--- app/general/routes.py
+++ app/general/routes.py
@@ -19,15 +19,17 @@ def home():
     Home page 
     """
     projects = Projects.query.limit(6).all()
-    software_projects = Projects.query.filter_by(project_type='SW').limit(6).all()
-    art_projects = Projects.query.filter_by(project_type='AR').limit(6).all()
-    latest_projects = Projects.query.filter_by(project_type='LT').limit(6).all()
-    upcoming_projects = Projects.query.filter_by(project_type='UPC').limit(6).all()
+    # software_projects = Projects.query.filter_by(project_type='SW').limit(6).all()
+    # art_projects = Projects.query.filter_by(project_type='AR').limit(6).all()
+    # latest_projects = Projects.query.filter_by(project_type='LT').limit(6).all()
+    # upcoming_projects = Projects.query.filter_by(project_type='UPC').limit(6).all()
 
     admin = User.query.get(1)
+    # return render_template('general/home.html', projects=projects, 
+    #     software_projects=software_projects, art_projects=art_projects, 
+    #     latest_projects=latest_projects, upcoming_projects=upcoming_projects, 
+    #     title='Welcome', admin=admin)
     return render_template('general/home.html', projects=projects, 
-        software_projects=software_projects, art_projects=art_projects, 
-        latest_projects=latest_projects, upcoming_projects=upcoming_projects, 
         title='Welcome', admin=admin)
 
 @general.route('/project/<int:id>', methods=['GET', 'POST'])
--- app/static/js/portfolio.js
+++ app/static/js/portfolio.js
@@ -1,48 +1,82 @@
 
-document.getElementById('overlay-click').addEventListener('click', function() {
-  document.querySelector('.bg-modal').style.display = 'flex'; 
-});
+// document.querySelector('.overlay-click').addEventListener('click', function() {
+//   document.querySelector('.bg-modal').style.display = 'flex'; 
+// });
 
-document.querySelector('.modal-close').addEventListener('click', function() {
-  document.querySelector('.bg-modal').style.display = 'none'; 
-})
+// document.querySelector('.modal-close').addEventListener('click', function() {
+//   document.querySelector('.bg-modal').style.display = 'none'; 
+// })
 
+const modalList = Array.from(document.querySelectorAll('.overlay'));
 
+modalList.forEach(function(item, index) {
 
-// Portfolio Slider
-$(document).ready(function(){
-  $('.portfolio-slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 10000,
+  var id = index + 1;
+  var modal = document.getElementById('modal'+id);
+  var modalId = modal.getAttribute('id');
+  var closeModal = modal.querySelector('.modal-close');
+
+  // Open the pop up project description on button click
+  item.addEventListener('click', function() {
+    document.querySelector('div#'+modalId+'.bg-modal').style.display = 'flex';
   });
+  // Close the pop up project description
+  closeModal.addEventListener('click', function() {
+    document.querySelector('div#'+modalId+'.bg-modal').style.display = 'none';
+  });
+
 });
 
 
-// Get the element with id="default-open" and click on it
-document.getElementById("defaultOpen").click();
 
-function displayProjects(evt, projectName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
+// modalButton.forEach(function(btn) {
+  
+//   btn.onclick(function() {
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+//     var modalId = document.getElementById('modal'+count);
+//     modalId.querySelector('.bg-modal').style.display = 'none'; 
+//   });
 
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
+//   count++;
 
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(projectName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
+// });
+
+
+
+// // Portfolio Slider
+// $(document).ready(function(){
+//   $('.portfolio-slider').slick({
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     autoplay: true,
+//     autoplaySpeed: 10000,
+//   });
+// });
+
+
+// // Get the element with id="default-open" and click on it
+// document.getElementById("defaultOpen").click();
+
+// function displayProjects(evt, projectName) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks;
+
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(projectName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
 
 
 
